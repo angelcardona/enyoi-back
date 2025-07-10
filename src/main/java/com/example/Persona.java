4a9642fffe95acf2940767b67f50acb1e7f50604
angelcardona <miguel.acardona98@gmail.com>
2025-07-10 09:40:50 -0500

    Ejercicio en clase de Rest

--- src/main/java/com/example/Persona.java
+++ src/main/java/com/example/Persona.java
@@ -1,5 +1,52 @@
-package com.example.demo;
+package com.example;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Table(name = "personas")
 public class Persona {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String correo;
+    private String edad;
+
+    
+    public Persona(Long id, String nombre, String correo, String edad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.correo = correo;
+        this.edad = edad;
+    }
+    public Persona() {
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getCorreo() {
+        return correo;
+    }
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+    public String getEdad() {
+        return edad;
+    }
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
 
 }
--- src/main/java/com/example/PersonaService.java
+++ src/main/java/com/example/PersonaService.java
@@ -1,5 +1,48 @@
 package com.example;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
 public class PersonaService {
+
+    private final PersonaRepository repository;
+
+    public PersonaService(PersonaRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Persona> findAll(){
+        return repository.findAll();
+    }
+    public Persona savePersona(Persona persona){
+        return repository.save(persona);
+    }
+    public Persona findById(Long id){
+        return repository.findById(id).orElse(null);
+
+    }
+    public Persona update(Long id,Persona persona){
+        return repository.findById(id).map(per ->{
+            per.setNombre(persona.getNombre());
+            per.setCorreo(persona.getCorreo());
+            per.setEdad(persona.getEdad());;
+            return repository.save(per);
+        }).orElseThrow(()-> new RuntimeException("Persona no encontrada"));
+    }
+    public void delete(Long id){
+        repository.deleteById(id);
+    }
+
+    public Persona updatePartial(PersonaDTO personaDTO,Long id){
+        return repository.findById(id).map(existingPerson ->{
+         personaDTO.getNombre().ifPresent(existingPerson::setNombre);
+         personaDTO.getCorreo().ifPresent(existingPerson::setCorreo);
+         personaDTO.getEdad().ifPresent(existingPerson::setEdad);
+            return repository.save(existingPerson);
+        }).orElseThrow(()->new RuntimeException("Persona no encontrada"));
+    }
+    
 
 }
